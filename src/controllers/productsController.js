9b9529735974aac1f16ae0ccbb4f10ceb21c99f5
lepzito <lucasrtinte19@gmail.com>
2023-08-18 10:54:39 -0300

    terminado CRUD II

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,19 +1,22 @@
 const fs = require("fs");
 const path = require("path");
-
 const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
-const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+function getProducts() {
+  return (products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8")));
+}
 
 /*const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");*/
 
 const controller = {
   // Root - Show all products
   index: (req, res) => {
+    const products = getProducts();
     res.render("products", { products: products });
   },
 
   // Detail - Detail from one product
   detail: (req, res) => {
+    const products = getProducts();
     let idProduct = req.params.id;
     let product = products.find((product) => product.id == idProduct);
     res.render("detail", { product: product });
@@ -21,43 +24,70 @@ const controller = {
 
   // Create - Form to create
   create: (req, res) => {
+    const products = getProducts();
     res.render("product-create-form", { products: products });
   },
 
   // Create -  Method to store
   store: (req, res) => {
-    const { name, price, image, description } = req.body;
-    const lastProductId =
-      products.length > 0 ? products[products.length - 1].id : 0;
-    const newProductId = lastProductId + 1;
+    const products = getProducts();
+    const { name, price, description, discount, category } = req.body;
+    const newProductId = products[products.length - 1].id + 1;
     const newProduct = {
       id: newProductId,
       name: name,
       price: parseFloat(price),
-      image: image,
+      image: req.file.filename,
       description: description,
+      discount: discount,
+      category: category,
     };
     products.push(newProduct);
-    fs.writeFileSync(
-      productsFilePath,
-      JSON.stringify(products, null, 2),
-      "utf-8"
-    );
-    res.redirect("/products");
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+    return res.redirect("/products");
   },
 
   // Update - Form to edit
   edit: (req, res) => {
-    // Do the magic
+    const products = getProducts();
+    const id = req.params.id;
+    const productToEdit = products.find((product) => product.id == id);
+    res.render("product-edit-form", { productToEdit: productToEdit });
   },
   // Update - Method to update
   update: (req, res) => {
-    // Do the magic
+    const products = getProducts();
+    const { name, price, description, discount, category } = req.body;
+    const id = req.params.id;
+
+    products.forEach((prod) => {
+      if (prod.id == id) {
+        prod.name = name !== "" ? name : prod.name;
+        prod.price = parseFloat(price) !== "" ? parseFloat(price) : prod.price;
+        prod.description = description !== "" ? description : prod.description;
+        prod.discount = discount !== "" ? discount : prod.discount;
+        prod.category = category !== "" ? category : prod.category;
+      }
+    });
+
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+    res.redirect(`/products/`);
   },
 
   // Delete - Delete one product from DB
   destroy: (req, res) => {
-    // Do the magic
+    const products = getProducts();
+
+    const id = req.params.id;
+
+    // Filtrar los productos para eliminar el producto con el ID especificado
+    const updatedProducts = products.filter((prod) => prod.id != id);
+
+    fs.writeFileSync(
+      productsFilePath,
+      JSON.stringify(updatedProducts, null, 2)
+    );
+    res.redirect("/products");
   },
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,16 @@
 // ************ Require's ************
 const express = require("express");
 const router = express.Router();
-
 // ************ Controller Require ************
 const productsController = require("../controllers/productsController");
+const upload = require("../middlewares/multer.js");
 
 /*** GET ALL PRODUCTS ***/
 router.get("/", productsController.index);
 
 /*** CREATE ONE PRODUCT ***/
 router.get("/create", productsController.create);
-router.post("/create", productsController.store);
+router.post("/", upload.single("productImage"), productsController.store);
 
 /*** GET ONE PRODUCT ***/
 router.get("/detail/:id", productsController.detail);
@@ -20,6 +20,6 @@ router.get("/:id/edit", productsController.edit);
 router.put("/:id", productsController.update);
 
 /*** DELETE ONE PRODUCT***/
-router.delete("/product/:id", productsController.destroy);
+router.delete("/:id", productsController.destroy);
 
 module.exports = router;
